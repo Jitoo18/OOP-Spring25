862c023518391ab27f667cceea9ce5df0fffc10f
Jitesh Kumar <162225212+Jitoo18@users.noreply.github.com>
2025-04-20 12:52:44 +0500

    Update LectureCourse.cpp

--- OOP LAB/LAB 9/Q3/LectureCourse.cpp
+++ OOP LAB/LAB 9/Q3/LectureCourse.cpp
@@ -1,11 +1,9 @@
 #include "LectureCourse.h"
-
 #include "iostream"
 using namespace std;
 
 LectureCourse::LectureCourse(string code, int credit, double mid, double final) 
  : Course (code, credit), midterm(mid),finalExam(final){}
-
 
 void LectureCourse::displayInfo()
 {
@@ -15,9 +13,12 @@ void LectureCourse::displayInfo()
 double LectureCourse::calculateGrade()
 {
     double Grade; 
-    if((midterm < 0 || midterm > 100) || (finalExam < 0 || finalExam > 100 )){
-        cout<<"Invalid"<<endl;
-    } else {
+    if((midterm < 0 || midterm > 100) || (finalExam < 0 || finalExam > 100 ))
+    {
+        cout << "Invalid" <<endl;
+    } 
+    else 
+    {
         Grade = midterm*0.4 + finalExam*0.6;
         cout << "Lecture Grade = " << Grade << endl;
     }
